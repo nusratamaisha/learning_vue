c166d52d33449b343d106a75a87baca21c9199a6
nusratamaisha <nusratamaisha@gmail.com>
2026-09-13 14:06:43 +0600

    feat: add routing project

--- Intermediate/component-communication/src/component/NewFriend.vue
+++ Intermediate/component-communication/src/component/NewFriend.vue
@@ -5,8 +5,8 @@
             <input type="text" v-model="enteredName">
         </div>
         <div>
-            <label>Phone</label>
-            <input type="tel" v-model="enteredPhone">
+            <label for="phone">Phone</label>
+            <input id="phone" type="tel" v-model="enteredPhone">
         </div>
         <div>
             <label>Email</label>
