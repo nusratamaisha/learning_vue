3498db9cf19181f592f8ce7aaf1592aad39128c7
nusratamaisha <nusratamaisha@gmail.com>
2026-09-15 14:02:12 +0600

    feat: build main-project with coaches, requests, and store modules

---
+++ Advance/main-project/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex';
+
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
+//import requests from './modules/requests/index.js';
+
+const store = createStore({
+    modules: {
+        coaches: coachesModule,
+        requests: requestsModule,
+    }, 
+    state(){
+        return{
+            userId: 'c3'
+        };
+    },
+    getters: {
+        userId(state){
+            return state.userId;
+        }
+    }
+});
+
+export default store;
